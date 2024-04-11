154db63b279d4e31db3fc327f82bf908e2896d0d
VickArmand <victormaina1962@gmail.com>
2024-04-11 19:09:57 +0300

    eslint

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,12 @@
+/*
+That returns a resolved promise with this object:
+{
+  firstName: value,
+  lastName: value,
+}
+*/
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve) => {
+    resolve({ firstName, lastName });
+  });
+}
