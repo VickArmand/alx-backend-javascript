68ffdd78072bc7621cb0cf3c3d91f20e73777a4b
VickArmand <victormaina1962@gmail.com>
2024-04-11 14:30:01 +0300

    adding comments

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,8 @@
+/* When the argument is:
+    true-> resolve the promise by passing an object with 2 attributes:status: 200, body: 'Success'
+    false -> reject the promise with an error object with the message
+    The fake API is not working currently
+*/
 export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'success' });
