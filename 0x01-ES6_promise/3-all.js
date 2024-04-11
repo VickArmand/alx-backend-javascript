f9bc5cba00027dbf4c48ca9ad280595362040ab9
VickArmand <victormaina1962@gmail.com>
2024-04-11 18:32:03 +0300

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+/*
+In this file, import uploadPhoto and createUser from utils.js
+
+Knowing that the functions in utils.js return promises,
+use the prototype below to collectively resolve all promises
+and log body firstName lastName to the console.
+
+In the event of an error, log Signup system offline to the console
+*/
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const createuser = createUser();
+  Promise.all([upload, createuser]).then((res) => { console.log(res[0].body, res[1].firstName, res[1].lastName); }, () => { console.log('Signup system offline'); });
+}
