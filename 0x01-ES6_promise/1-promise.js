294e03b413e4993118488e7304a75f815e3e43d3
VickArmand <victormaina1962@gmail.com>
2024-04-11 20:04:09 +0300

    corrections

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@
 */
 export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'success' });
+    if (success) resolve({ status: 200, body: 'Success' });
     else reject(Error('The fake API is not working currently'));
   });
   return promise;
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,8 +5,6 @@ When the Promise rejects, return an empty Error object
 For every resolution, log Got a response from the API to the console
 */
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return { status: 200, body: 'success' };
-  }, () => Error());
+  promise.then(() => ({ status: 200, body: 'success' }),
+    () => Error()).finally(() => { console.log('Got a response from the API'); });
 }
