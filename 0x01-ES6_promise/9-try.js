31525f7f2f5277cc07081324a89bcd24c951a170
VickArmand <victormaina1962@gmail.com>
2024-04-13 11:34:52 +0300

    corrections

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -7,11 +7,13 @@ If this function throws an error, the error message should be appended to the qu
 In every case, the message Guardrail was processed should be added to the queue.
  */
 export default function guardrail(mathFunction) {
-  const queue = ['Guardrail was processed'];
+  const queue = [];
   try {
     queue.push(mathFunction());
   } catch (error) {
     queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
   return queue;
 }
