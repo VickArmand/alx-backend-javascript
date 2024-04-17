81db68446500e4abf67148ba2b52425cbf0c22a0
VickArmand <victormaina1962@gmail.com>
2024-04-17 21:43:45 +0300

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+/**
+ * Create a function named groceriesList that
+ * returns a map of groceries with the following items (name, quantity):
+ */
+export default function groceriesList() {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+}
