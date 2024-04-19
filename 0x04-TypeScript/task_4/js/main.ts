85923807bfb89acc48d074298d13416eda0448d4
VickArmand <victormaina1962@gmail.com>
2024-04-19 14:31:17 +0300

    Update task_4/js/main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,10 @@
+namespace Subjects {
+    export const cpp = Cpp;
+    export const java = Java;
+    export const react = React;
+    export const cTeacher: Teacher = {
+        firstName: "test",
+        lastName: "test",
+        experienceTeachingC: 10,
+    }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,15 @@
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number;
+    }
+    export class Java {
+        teacher: Teacher;
+        getRequirements(): string {
+            return "Here is the list of requirements for Java";
+        }
+        getAvailableTeacher(): string {
+            if (this.teacher.experienceTeachingJava === 0) return "No available teacher";
+            return `Available Teacher: ${this.teacher.firstName}`;
+        }
+    }
+}
