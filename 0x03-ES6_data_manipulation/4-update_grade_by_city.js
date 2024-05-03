ca05e140a7313b34ba6e800a0aa5ed4423362eb4
VickArmand <victormaina1962@gmail.com>
2024-05-03 14:09:06 +0300

    corrections 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -8,19 +8,15 @@
  * If a student doesn’t have grade in newGrades, the final grade should be N/A.
  * You must use filter and map combined.
  */
-function updateObject(studentObject, gradeObject) {
-  Object.entries(gradeObject).map((entry) => {
-    if (entry[0] === 'studentId' && entry[1] === studentObject.id) {
-      studentObject.grade = gradeObject.grade;
-    }
-    return studentObject;
-  });
-}
-
-export default function updateStudentGradeByCity(array, city, newGrades) {
-  const result = array.filter((student) => student.location === city);
-  result.forEach((student) => {
-    newGrades.map((grade) => updateObject(student, grade));
-  });
-  return result.map((student) => { if (student.grade === undefined) student.grade = 'N/A'; return student; });
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((el) => el.location === city)
+    .map((person) => {
+      const grades = newGrades.filter((el) => el.studentId === person.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...person,
+        grade,
+      };
+    });
 }
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -6,10 +6,10 @@
  * If adding the value is not possible the error Position outside range should be thrown.
  */
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const arr = new Int8Array(buffer);
   if (position >= length) throw Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const newView = new DataView(buffer, 0, length);
+  const arr = new Int8Array(buffer);
   arr[position] = value;
-  return arr;
-//
+  return newView;
 }
