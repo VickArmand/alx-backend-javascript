94141ae0c48230ebc704e1ebfb9ef353554af9c3
VickArmand <victormaina1962@gmail.com>
2024-06-05 03:51:21 +0300

    corrections

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,5 @@
 // create a function named displayMessage
 // that prints in STDOUT the string argument.
-const process = require('process');
 module.exports = function displayMessage (message) {
-  process.stdout.write(`${message}\n`);
+  console.log(message);
 };
