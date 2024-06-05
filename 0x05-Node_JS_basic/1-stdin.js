0c12d5c438e21e4357f5c7144105638309237975
VickArmand <victormaina1962@gmail.com>
2024-06-05 21:35:31 +0300

    lint corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,11 +4,13 @@
  * Welcome to Holberton School, what is your name? (followed by a new line)
  * The user should be able to input their name on a new line
  * The program should display Your name is: INPUT
- * When the user ends the program, it should display This important software is now closing (followed by a new line)
+ * When the user ends the program,
+ * it should display This important software is now closing (followed by a new line)
  */
 const process = require('process');
+
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
