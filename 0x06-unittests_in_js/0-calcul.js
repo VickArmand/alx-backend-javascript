6a4e920675c4701845c31188e7bceb5bfc04d376
VickArmand <victormaina1962@gmail.com>
2024-06-07 05:30:48 +0300

    add 0-calcul

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,10 @@
+/**
+ * calculateNumber - round a and b and
+ * return the sum of it
+ * @param {*} a 
+ * @param {*} b 
+ * @returns sum
+ */
+function calculateNumber(a, b) {
+  return (Math.round(a) + Math.round(b))
+}
