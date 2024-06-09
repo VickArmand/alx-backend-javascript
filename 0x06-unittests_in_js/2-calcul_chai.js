7265472857fc5cc34469a762d4b40050539ace89
VickArmand <victormaina1962@gmail.com>
2024-06-09 12:12:15 +0300

    2. Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,35 @@
+/**
+ * calculateNumber - round a and b and
+ * return the sum of it 
+ * type can be SUM, SUBTRACT, or DIVIDE (string)
+ * When type is SUM, round the two numbers, and add a and b
+ * When type is SUBTRACT, round the two numbers, and subtract b from a
+ * When type is DIVIDE, round the two numbers, and
+ * divide a with b - if the rounded value of b is equal to 0,
+ * return the string Error
+ */
+module.exports = function calculateNumber(type, a, b) {
+    const a1 = Math.round(a);
+    const b1 = Math.round(b);
+    let c = 0;
+    switch(type) {
+      case 'SUM':
+        c = (a1 + b1);
+        break;
+      case 'SUBTRACT':
+        c = a1 - b1;
+        break;
+      case 'DIVIDE':
+        if (b1 === 0)
+          c = 'Error';
+        else {
+          c = a1 / b1;
+        }
+        break;
+      default:
+        c  = 'Invalid Operation';
+        break;
+    }
+    return c;
+  }
+  
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,57 @@
+/**
+ * this is a test file containing tests for
+ * calculateNumber function
+ */
+const expect = require('chai').expect;
+const calculateNumber = require('./1-calcul.js');
+
+describe('sum-test-with-type', () => {
+  it('positive-numbers', () => {
+    expect(calculateNumber('SUM', 2.5, 3.5)).to.be.equal(7);
+  });
+  it('positive-numbers-2', () => {
+    expect(calculateNumber('SUM', 8.4, 2.1)).to.be.equal(10);
+  });
+  it('negative-numbers', () => {
+    expect(calculateNumber('SUM', -2.5, -3.5)).to.be.equal(-5);
+  });
+  it('negative-numbers-2', () => {
+    expect(calculateNumber('SUM', -5.3, -8.6)).to.be.equal(-14);
+  });
+});
+describe('subtract-test-with-type', () => {
+  it('positive-numbers', () => {
+    expect(calculateNumber('SUBTRACT', 2.5, 3.5)).to.be.equal(-1);
+  });
+  it('positive-numbers-2', () => {
+    expect(calculateNumber('SUBTRACT', 2.1, 8.4)).to.be.equal(-6);
+  });
+  it('negative-numbers', () => {
+    expect(calculateNumber('SUBTRACT', -3.5, -2.5)).to.be.equal(-1);
+  });
+});
+describe('divide-test-with-type', () => {
+  it('positive-numbers', () => {
+    expect(calculateNumber('DIVIDE', 2.5, 3.5)).to.be.equal(0.75);
+  });
+  it('positive-numbers-2', () => {
+    expect(calculateNumber('DIVIDE', 8.4, 2.1)).to.be.equal(4);
+  });
+  it('negative-numbers', () => {
+    expect(calculateNumber('DIVIDE', -2.5, -3.5)).to.be.equal((2/3));
+  });
+  it('negative-numbers-2', () => {
+    expect(calculateNumber('DIVIDE', -8.6, 0)).to.be.equal('Error');
+  });
+});
+describe('invalid-test-with-type', () => {
+  it('positive-numbers', () => {
+    expect(calculateNumber('MOD', 2.5, 3.5)).to.be.equal('Invalid Operation');
+  });
+  it('positive-numbers-2', () => {
+    expect(calculateNumber('POW', 8.4, 2.1)).to.be.equal('Invalid Operation');
+  });
+  it('positive-numbers-2', () => {
+    expect(calculateNumber('MULTIPLY', 8.4, 2.1)).to.be.equal('Invalid Operation');
+  });
+});
