06308f85e473112af6beb2c348c4b23c09c775e8
VickArmand <victormaina1962@gmail.com>
2024-06-10 06:03:40 +0300

    corrections

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,19 @@
 const request = require('request');
 const assert = require('assert');
 
-describe('api-test', () => {
+describe('api-test-code', () => {
   it('test-status-code', (done) => {
-    request('/', (err, res, body) => {
+    request('http://localhost:7865/', (err, res, body) => {
       assert.equal(res.statusCode, 200);
       done();
     });
   });
+});
+describe('api-test-response', () => {
   it('test-response', (done) => {
-    request('/', (err, res, body) => {
-      assert.equal(body, 'Welcome to the payment system');
-      done();
-    });
+    request('http://localhost:7865/', (err, res, body) => {
+    assert.equal(body, 'Welcome to the payment system');
+    done();
   });
+});
 });
