30912ad12a30d5b99d5ea5990e95ca297c764371
VickArmand <victormaina1962@gmail.com>
2024-06-11 02:01:14 +0300

    9. Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Welcome to the payment system');
+});
+app.get('/cart/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (id) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(`Payment methods for cart ${req.params.id}`);
+  }
+  else {
+    res.statusCode = 403;
+    res.end('Id must be int');
+  }
+});
+app.listen(7865);
+console.log('API available on localhost port 7865');
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const assert = require('assert');
+
+describe('api-test-param', () => {
+  it('id is not a number', (done) => {
+    request('http://localhost:7865/cart/joe', (err, res, body) => {
+      assert.equal(res.statusCode, 403);
+      assert.equal(body, 'Id must be int');
+      done();
+    });
+  });
+});
+describe('api-test', () => {
+  it('id is a number', (done) => {
+    request('http://localhost:7865/cart/22', (err, res, body) => {
+      assert.equal(body, 'Payment methods for cart 22');
+      assert.equal(res.statusCode, 200);
+      done();
+  });
+});
+});
