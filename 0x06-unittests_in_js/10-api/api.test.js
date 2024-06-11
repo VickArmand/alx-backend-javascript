9e96f8b84ec0a9266335f98be1df354b68190eb1
VickArmand <victormaina1962@gmail.com>
2024-06-11 03:14:08 +0300

    corrections

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -24,3 +24,39 @@ describe('api-test-available_payments', () => {
   });
 });
 });
+
+describe('api-test-param', () => {
+  it('id is not a number', (done) => {
+    request('http://localhost:7865/cart/joe', (err, res, body) => {
+      assert.equal(res.statusCode, 404);
+      assert.equal(body, 'Id must be int');
+      done();
+    });
+  });
+});
+describe('api-test', () => {
+  it('id is a number', (done) => {
+    request('http://localhost:7865/cart/22', (err, res, body) => {
+      assert.equal(body, 'Payment methods for cart 22');
+      assert.equal(res.statusCode, 200);
+      done();
+  });
+});
+});
+
+describe('api-test-code', () => {
+  it('test-status-code', (done) => {
+    request('http://localhost:7865/', (err, res, body) => {
+      assert.equal(res.statusCode, 200);
+      done();
+    });
+  });
+});
+describe('api-test-response', () => {
+  it('test-response', (done) => {
+    request('http://localhost:7865/', (err, res, body) => {
+    assert.equal(body, 'Welcome to the payment system');
+    done();
+  });
+});
+});
