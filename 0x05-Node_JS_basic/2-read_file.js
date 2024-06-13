c5bb9cd5c93d3455b5132c1e43b7d92c17f86111
VickArmand <victormaina1962@gmail.com>
2024-06-14 02:31:52 +0300

    changes

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,38 @@
-/**
- * Create a function named countStudents. It should accept a path in argument
- * The script should attempt to read the database file synchronously
- * If the database is not available, it should throw an error with the text
- * Cannot load the database
- * If the database is available, it should log the following message to the
- * console Number of students: NUMBER_OF_STUDENTS
- * It should log the number of students in each field, and the list with the following format:
- * Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES
- * CSV file can contain empty lines (at the end) - and they are not a valid student!
- */
 const fs = require('fs');
 
-module.exports = function countStudents(path) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const contents = data.split('\n');
-    console.log(`Number of students: ${contents.length - 1}`);
-    const students = {};
-    for (let i = 1; i < contents.length; i += 1) {
-      const row = contents[i].split(',');
-      students[row[3]] = [];
-    }
-    contents.forEach((content) => {
-      const row = content.split(',');
-      if (row[3] in students) {
-        students[row[3]].push(row[0]);
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
-    });
-    Object.keys(students).forEach((key) => {
-      console.log(`Number of students in ${key}: ${students[key].length}. List: ${students[key].join(', ')}`);
-    });
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
   } catch (error) {
     throw Error('Cannot load the database');
   }
-};
+}
+
+module.exports = countStudents;
